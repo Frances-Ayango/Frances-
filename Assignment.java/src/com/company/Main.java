716d2b1cb3e97e7eb773015cb1ca7a6345596428
Frances-Ayango <88836480+Frances-Ayango@users.noreply.github.com>
2021-08-12 17:52:20 +0300

    Add files via upload

---
+++ Assignment.java/src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int KCSEGrade=65;
+        int interviewScore=6;
+        int confidenceScore=5;
+        if(KCSEGrade>=65);
+        {
+            if(interviewScore>=6);
+            System.out.println(" KCSE GRADE: "+65);
+        }else
+            System.out.println("Interview question score is "+6);
+        {
+            if(confidenceScore>=5);
+            System.out.println("the confidence leve score is "+5);
+        }
+    }
+}
